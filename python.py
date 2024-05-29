870535f2d7c91af5ee1cd5d7219f39148851b3c5
SaniyaFarooqui <saniyafarooqui05@gmail.com>
2024-05-29 23:54:15 +0530

    started python

---
+++ python.py
@@ -0,0 +1,28 @@
+x = float(1);
+y= int(5);
+print (x)
+print(type(y))
+print ("hello world!")
+
+b ="awesome!" 
+
+def myfunc():
+    print("python is " + b)
+
+myfunc()
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+def myfunc(): 
+   global y
+   y ="fantastic"
+myfunc()
+print ("python is "+y)
